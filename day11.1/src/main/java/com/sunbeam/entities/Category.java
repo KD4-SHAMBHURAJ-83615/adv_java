f683a3b579669d86c9088bf0281fc92e46048cb5
KD4_Shambhuraj_83615 <159098785+shambhurajlavand@users.noreply.github.com>
2024-07-03 17:21:23 +0530

    day 11 uploaded

---
+++ day11.1/src/main/java/com/sunbeam/entities/Category.java
@@ -0,0 +1,95 @@
+package com.sunbeam.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity {
+	@Column(name = "name", length = 30, unique = true)
+	private String categoryName;
+	private String description;
+	// Category 1--->* BlogPost
+	//one ,parent non owning (since it does'nt contain FK)
+	
+	
+	/*
+	 * 
+	 * manpped by -mandatory in bi dir association btween entities
+	 * appearans in inverse (non owning )
+	 * 
+	 * default fetch type any to many -lazy
+	 * while accesing un-fetched data (i.e proxy) outside of the session 
+	 * hibernate throws -lazyInitializationExeption
+	 * orphan removal = true = tell hibernate delete the record from the 
+	 * the movements its removed from association
+	 */
+	@OneToMany(mappedBy = "selectedCategory", 
+			cascade = CascadeType.ALL/* ,fetch = FetchType.EAGER */,
+			orphanRemoval = true)
+	
+	private List<BlogPost> posts = new ArrayList<>();
+
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(String categoryName, String description) {
+		super();
+		this.categoryName = categoryName;
+		this.description = description;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<BlogPost> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<BlogPost> posts) {
+		this.posts = posts;
+	}
+
+	// add helper method in the entity layer to establish bi-dir association between
+	// Category n post
+	public void addBlogPost(BlogPost post) {
+		// establish category --> post (parent --> child)
+		this.posts.add(post);
+		// establish post --> category (child -> parent)
+		post.setSelectedCategory(this);
+	}
+
+	// add helper method in the entity layer to un - establish bi-dir association
+	// between Category n post
+	public void removeBlogPost(BlogPost post) {
+		this.posts.remove(post);
+		post.setSelectedCategory(null);
+	}
+
+	@Override
+	public String toString() {
+		return "Category ID " + getId() + " [categoryName=" + categoryName + ", description=" + description + "]";
+	}
+
+}
